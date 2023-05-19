00601a7522e1708b290224852eff8cafda7c43ef
Elgin88 <111440347+Elgin88@users.noreply.github.com>
2023-05-19 13:30:50 +0800

    made flash fuel lamp

--- Assets/FuelBarIndicateFuelLess.cs
+++ Assets/FuelBarIndicateFuelLess.cs
@@ -2,14 +2,17 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 [RequireComponent(typeof(RectTransform))]
+[RequireComponent(typeof(Image))]
 
 public class FuelBarIndicateFuelLess : MonoBehaviour
 {
     [SerializeField] private float _minLevelForIndicate;
     [SerializeField] private float _duration;
     [SerializeField] private float _deltaScale;
+    [SerializeField] private Color _targetColor;
 
     private PlayerFuelController _fuelController;
     private RectTransform _rectTransform;
@@ -17,16 +20,20 @@ public class FuelBarIndicateFuelLess : MonoBehaviour
     private Vector3 _startStale;
     private Vector3 _currentScale;
     private Vector3 _targetScale;
+    private Color _startColor;
+    private Image _image;
     private float _currentFuel;
     private float _maxFuel;
-    private float _delta;
+    private float _deltaScaleCalculated;
 
     private void OnEnable()
     {
         _fuelController = FindObjectOfType<PlayerFuelController>();
         _rectTransform = GetComponent<RectTransform>();
+        _image = GetComponent<Image>();
 
         _startStale = _rectTransform.localScale;
+        _startColor = _image.color;
 
         _currentScale = _startStale;
 
@@ -34,9 +41,15 @@ public class FuelBarIndicateFuelLess : MonoBehaviour
         _targetScale.y = _startStale.y + _deltaScale;
         _targetScale.z = _startStale.z + _deltaScale;
 
-        _delta = _deltaScale / (_duration / 2 / Time.deltaTime);
+        _deltaScaleCalculated = _deltaScale / (_duration / 2 / Time.deltaTime);
 
         _fuelController.IsFuelChanged += OnFuelChanged;
+
+
+        
+
+
+
     } 
 
     private void OnDisable()
@@ -63,9 +76,11 @@ public class FuelBarIndicateFuelLess : MonoBehaviour
         {
             if (_currentScale.x < _targetScale.x & isBack == false)
             {
-                _currentScale.x = Mathf.MoveTowards(_currentScale.x, _targetScale.x, _delta);
-                _currentScale.y = Mathf.MoveTowards(_currentScale.y, _targetScale.y, _delta);
-                _currentScale.z = Mathf.MoveTowards(_currentScale.z, _targetScale.z, _delta);
+                _currentScale.x = Mathf.MoveTowards(_currentScale.x, _targetScale.x, _deltaScaleCalculated);
+                _currentScale.y = Mathf.MoveTowards(_currentScale.y, _targetScale.y, _deltaScaleCalculated);
+                _currentScale.z = Mathf.MoveTowards(_currentScale.z, _targetScale.z, _deltaScaleCalculated);
+
+                _image.CrossFadeColor(_targetColor, _duration / 2, true, false);
             }
             else
             {
@@ -74,9 +89,11 @@ public class FuelBarIndicateFuelLess : MonoBehaviour
 
             if (_currentScale.x > _startStale.x & isBack == true)
             {
-                _currentScale.x = Mathf.MoveTowards(_currentScale.x, _startStale.x, _delta);
-                _currentScale.y = Mathf.MoveTowards(_currentScale.y, _startStale.y, _delta);
-                _currentScale.z = Mathf.MoveTowards(_currentScale.z, _startStale.z, _delta);
+                _currentScale.x = Mathf.MoveTowards(_currentScale.x, _startStale.x, _deltaScaleCalculated);
+                _currentScale.y = Mathf.MoveTowards(_currentScale.y, _startStale.y, _deltaScaleCalculated);
+                _currentScale.z = Mathf.MoveTowards(_currentScale.z, _startStale.z, _deltaScaleCalculated);
+
+                _image.CrossFadeColor(_startColor, _duration / 2, true, false);
             }
             else
             {
@@ -85,6 +102,9 @@ public class FuelBarIndicateFuelLess : MonoBehaviour
 
             if (_currentFuel/_maxFuel > _minLevelForIndicate)
             {
+                _image.color = _startColor;
+                _currentScale = _startStale;
+
                 StopCoroutine(_flash);
             }
 
